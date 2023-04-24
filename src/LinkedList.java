a1be6fdb6156b70d87b6e272486be5f486af3faf
Ashish Pal <ak987982@gmail.com>
2023-04-24 19:37:57 +0530

    Added UC3 append user case

--- src/LinkedList.java
+++ src/LinkedList.java
@@ -14,14 +14,11 @@ public class LinkedList {
     public static void main(String[] args) {
         LinkedList list = new LinkedList();
         System.out.println("Welcome to Linked List Practice Problem");
-
-        list.head = new Node(70);
+        list.head = new Node(56);
         Node second = new Node(30);
-        Node third = new Node(56);
-
+        Node third = new Node(70);
         list.head.next = second;
         second.next = third;
-
         Node n = list.head;
         while (n != null) {
             System.out.print(n.data + " ");
